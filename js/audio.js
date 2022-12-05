ee625b58f5f59bd0913d14767d1d0f020cac8edf
zhiangzz <91102941+zhiangzz@users.noreply.github.com>
2022-12-05 15:42:59 -0500

    Add files via upload

--- js/audio.js
+++ js/audio.js
@@ -9,13 +9,13 @@ audio.loop=false;
 //         volume.innerHTML=0;
 //     }
 // })
-const check = ()=>{
-    if (audio.muted===true){
-    slider.value = 0;
-    volume.innerHTML=0;
-}
-}
-check()
+// const check = ()=>{
+//     if (audio.muted===true){
+//     slider.value = 0;
+//     volume.innerHTML=0;
+// }
+// }
+// check()
 slider.addEventListener('change',(e)=>{
     audio.volume = e.target.value/100;
     volume.innerHTML = `${audio.volume*100}%`;
